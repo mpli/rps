7cf3c8d34a094b85911ff78ded77bd4f3dfe9edf
softdev-zeus <softdev.zeus@gmail.com>
2017-07-04 10:36:19 -0700

    bug fix3 of menu collapse

--- src/+app/app.component.ts
+++ src/+app/app.component.ts
@@ -40,14 +40,48 @@ export class XLargeDirective {
   template: `
   <img itemprop="image" src="../assets/image/roseville_pool_service_logo5.png" alt="Roseville Pool Service">
 
+<header class="bs-docs-nav navbar navbar-static-top" id="top">
+    <div class="container">
+      <div class="navbar-header">
+        <button aria-controls="bs-navbar" aria-expanded="true" class="navbar-toggle" data-target="#bs-navbar" data-toggle="collapse" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a href="../" class="navbar-brand">Bootstrap</a> </div>
+      <nav class="navbar-collapse collapse in" id="bs-navbar" aria-expanded="true">
+        <ul class="nav navbar-nav">
+          <li> <a href="../getting-started/">Getting started</a>
+          </li>
+          <li> <a href="../css/">CSS</a>
+          </li>
+          <li> <a href="../components/">Components</a>
+          </li>
+          <li> <a href="../javascript/">JavaScript</a>
+          </li>
+          <li> <a href="../customize/">Customize</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="http://themes.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Themes&quot;)">Themes</a></li>
+          <li><a href="https://jobs.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Jobs&quot;)">Jobs</a></li>
+          <li><a href="http://expo.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Expo&quot;)">Expo</a></li>
+          <li><a href="http://blog.getbootstrap.com" onclick="ga(&quot;send&quot;,&quot;event&quot;,&quot;Navbar&quot;,&quot;Community links&quot;,&quot;Blog&quot;)">Blog</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
   <div class="navbar navbar-default" id="menu">
       <div class="navbar-header">
+        <button aria-controls="bs-navbar" aria-expanded="true" class="navbar-toggle" data-target="#bs-navbar" data-toggle="collapse" type="button">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="../" class="navbar-brand">Bootstrap</a>
           <!-- <a class="navbar-brand" href="index.html">Roseville Pool Service</a> -->
           <ul class="nav navbar-nav pull-right visible-xs-block">
               <li>
-                <a data-toggle="collapse" data-target="#navbar-mobile">
+                <button data-toggle="collapse" data-target="#navbar-mobile">
                   <i class="fa fa-bars" id="hamburger-menu" aria-hidden="true"></i>
-                </a>
+                </button>
               </li>
           </ul>
       </div>
