f6773dbf02ff8d0e517d146c1f69188c29a323d6
softdev-zeus <softdev.zeus@gmail.com>
2017-08-13 12:09:54 -0700

    indexing

---
+++ src/server.redirection.ts
@@ -0,0 +1,31 @@
+interface redirection {
+    from: string,
+    to: string,
+}
+
+export const redirections: redirection[] =  [
+    {
+        from: 'roseville_pool_service__commitment.html',
+        to: 'commit',
+    },
+    {
+        from: 'RosevillePoolService_Testimonials.html',
+        to: 'reviews',
+    },
+    {
+        from: 'roseville-pool_service__contact_us.html',
+        to: 'contact-us',
+    },
+    {
+        from: 'roseville_pool_service__pool_tips.html',
+        to: 'pool-tips',
+    },
+    {
+        from: 'roseville_pool_service__links.html',
+        to: 'links',
+    },
+    {
+        from: 'roseville_pool_service__weekly_services.html',
+        to: 'weekly-services',
+    },
+];
